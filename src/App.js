90895ab9d5d9e9e5409831a81e620aecbbda5c67
AbhirupDey12 <abhirupd470@gmail.com>
2023-02-26 23:28:30 +0530

    begin

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
 import './App.css' ;
 
 function App() {
   return (
-    <div className="app">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="main">
-
-      </div>
-      <div className="footer">
-
-      </div>
+    <div>
+      Hello world!
     </div>
   );
 }
