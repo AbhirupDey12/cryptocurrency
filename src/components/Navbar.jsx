3d0957991b7ec52432e6afa41939ff49d7aba62c
AbhirupDey12 <abhirupd470@gmail.com>
2023-02-26 23:25:23 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () =>  {
 
                <div className="logo-container">
 
-                    <Avatar src={icon} size="large" />
+                              <Avatar src={icon} size="large" />
 
                     <Typography.Title level={2} className='logo'>
 
